68b08722b5881b5becce6e21fa62e5285f5e4c5b
jiajiawang <1273570695@qq.com>
2019-03-28 22:16:05 +0800

    solve problem "Climbing Stairs"

---
+++ java/cn/keaper/p70/Solution.java
@@ -0,0 +1,33 @@
+package cn.keaper.p70;
+
+/**
+ * https://leetcode.com/problems/climbing-stairs/
+ */
+public class Solution {
+
+    /**
+     * recursion approach will out of time limit
+     * iteration is faster
+     * actually, tail recursion can be transformed iteration
+     */
+    public int climbStairs(int n) {
+        if(n == 1 || n == 0){
+            return 1;
+        }
+        int a = 1,b = 1, c = 2;
+        while (n - 2 > 0){
+            a = b;
+            b = c;
+            c = a + b;
+            n--;
+        }
+        return  c;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        for (int i = 1 ; i < 45 ; i ++){
+            System.out.println(solution.climbStairs(i));
+        }
+    }
+}

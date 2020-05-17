a670a4325f5c65eccb3412f132d1fb75c37a1ad8
jiajiawang <1273570695@qq.com>
2020-05-17 21:31:33 +0800

    other solution for problem "Word Pattern"

--- algorithms/java/src/cn/keaper/p290/Solution.java
+++ algorithms/java/src/cn/keaper/p290/Solution.java
@@ -36,4 +36,31 @@ public class Solution {
         }
         return true;
     }
+
+
+    /**
+     * 更容易理解的方式
+     * @see cn.keaper.p205.Solution#isIsomorphic4(String, String)
+     */
+    public boolean wordPattern2(String pattern, String str) {
+        Map<Character,String> k2v = new HashMap<>();
+        Map<String,Character> v2k = new HashMap<>();
+        char[] patternArray = pattern.toCharArray();
+        String[] strArray = str.split(" ");
+        if(patternArray.length != strArray.length){
+            return false;
+        }
+        for (int i = 0; i < patternArray.length; i++) {
+            if(k2v.containsKey(patternArray[i]) && !k2v.get(patternArray[i]).equals(strArray[i])){
+                return false;
+            }
+            if(v2k.containsKey(strArray[i]) && !v2k.get(strArray[i]).equals(patternArray[i])){
+                return false;
+            }
+            k2v.put(patternArray[i],strArray[i]);
+            v2k.put(strArray[i],patternArray[i]);
+        }
+        return true;
+    }
+
 }

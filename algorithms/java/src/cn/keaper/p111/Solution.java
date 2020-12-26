37c3698f362437c5583d2678428b4bd7327e7d88
jiajiawang <1273570695@qq.com>
2020-12-27 03:15:46 +0800

    other solution of "Minimum Depth of Binary Tree"

--- algorithms/java/src/cn/keaper/p111/Solution.java
+++ algorithms/java/src/cn/keaper/p111/Solution.java
@@ -2,6 +2,9 @@ package cn.keaper.p111;
 
 import cn.keaper.utils.tree.integer.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * https://leetcode.com/problems/minimum-depth-of-binary-tree/
  */
@@ -21,5 +24,34 @@ public class Solution {
             return 1 + minDepth(root.right);
         }
         return 1 + minDepth(root.left);
+    }
+
+    /**
+     * BFS
+     */
+    public int minDepth1(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int level = 1;
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            while (size-- > 0){
+                TreeNode poll = queue.poll();
+                if(poll.left == null && poll.right == null){
+                    return level;
+                }
+                if(poll.left != null){
+                    queue.offer(poll.left);
+                }
+                if(poll.right != null){
+                    queue.offer(poll.right);
+                }
+            }
+            level++;
+        }
+        return level;
     }
  }
--- algorithms/java/src/cn/keaper/p111/SolutionTest.java
+++ algorithms/java/src/cn/keaper/p111/SolutionTest.java
@@ -3,14 +3,27 @@ package cn.keaper.p111;
 import cn.keaper.utils.tree.integer.TreeBuilder;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SolutionTest {
+
+    private Solution solution = new Solution();
+
+    private List<Integer[]> cases = Arrays.asList(
+            new Integer[]{3, 9, 20, null, null, 15, 7},
+            new Integer[]{3, 9},
+            new Integer[]{3},
+            new Integer[]{}
+    );
 
     @Test
     public void minDepth() {
-        Solution solution = new Solution();
-        System.out.println(solution.minDepth(TreeBuilder.buildTree(new Integer[]{3,9,20,null,null,15,7})));
-        System.out.println(solution.minDepth(TreeBuilder.buildTree(new Integer[]{3,9})));
-        System.out.println(solution.minDepth(TreeBuilder.buildTree(new Integer[]{3})));
-        System.out.println(solution.minDepth(TreeBuilder.buildTree(new Integer[]{})));
+        cases.forEach(c -> {
+            System.out.println(solution.minDepth(TreeBuilder.buildTree(c)));
+            System.out.println(solution.minDepth1(TreeBuilder.buildTree(c)));
+            System.out.println();
+        });
+
     }
 }

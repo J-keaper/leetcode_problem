8cf2d9371fc43403cacaef81dde14dd121f6eb14
jiajiawang <1273570695@qq.com>
2021-01-06 02:01:19 +0800

    modify print tree method

--- algorithms/java/src/cn/keaper/utils/tree/integer/TreePrinter.java
+++ algorithms/java/src/cn/keaper/utils/tree/integer/TreePrinter.java
@@ -1,28 +1,36 @@
 package cn.keaper.utils.tree.integer;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class TreePrinter {
 
     public static void printTree(TreeNode root){
-        if(root == null){
-            return;
-        }
+        List<String> sequence = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()){
-            TreeNode node = queue.poll();
-            if(node != null){
-                System.out.print(node.val + ",");
-            }else {
-                System.out.print("null,");
-                continue;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode poll = queue.poll();
+            if (poll != null) {
+                sequence.add(String.valueOf(poll.val));
+                queue.offer(poll.left);
+                queue.offer(poll.right);
+            } else {
+                sequence.add(null);
             }
-            queue.add(node.left);
-            queue.add(node.right);
         }
+        int last = sequence.size() - 1;
+        while (sequence.get(last) == null) {
+            last--;
+        }
+        System.out.println(String.join(",", sequence.subList(0, last + 1)));
     }
 
+
+    public static void main(String[] args) {
+      TreePrinter.printTree(TreeBuilder.buildTree(new Integer[]{1,2,3,null,null,4,5}));
+    }
 
 }

064b3238487a5b498ff25e23073f93f5da780ee2
jiajiawang <1273570695@qq.com>
2021-01-26 00:30:13 +0800

    other solution of problem "Search in Rotated Sorted Array"

--- algorithms/java/src/cn/keaper/p33/Solution.java
+++ algorithms/java/src/cn/keaper/p33/Solution.java
@@ -33,4 +33,57 @@ public class Solution {
         return nums[l] == target ? l : nums[r] == target ? r : -1;
     }
 
+
+
+    public int search1(int[] nums, int target) {
+        if(nums.length == 0){
+            return -1;
+        }
+        int l = 0, r = nums.length - 1;
+        while (l < r){
+            int m = (l + r) >>> 1;
+            if(nums[m] >= nums[l]){
+                if(nums[l] <= target && target <= nums[m]){
+                    r = m;
+                }else {
+                    l = m + 1;
+                }
+            }else{
+                if(nums[m] < target && target <= nums[r]){
+                    l = m + 1;
+                }else{
+                    r = m;
+                }
+            }
+        }
+        return nums[l] == target ? l : -1;
+    }
+
+
+    public int search2(int[] nums, int target) {
+        if(nums.length == 0){
+            return -1;
+        }
+        int l = 0, r = nums.length - 1;
+        while (l <= r){
+            int m = (l + r) >>> 1;
+            if(nums[m] == target){
+                return m;
+            }
+            if(nums[m] >= nums[l]){
+                if(nums[l] <= target && target < nums[m]){
+                    r = m - 1;
+                }else {
+                    l = m + 1;
+                }
+            }else{
+                if(nums[m] < target && target <= nums[r]){
+                    l = m + 1;
+                }else {
+                    r = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
